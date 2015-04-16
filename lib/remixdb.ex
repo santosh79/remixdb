d696f9bf73dc8c63b12b9561b5403a5adc0b6f45
Santosh Kumar <santosh79@gmail.com>
2015-04-16 09:56:37 -0400

    added get and set functions that do nothing now

--- lib/remixdb.ex
+++ lib/remixdb.ex
@@ -1,2 +1,13 @@
 defmodule Remixdb do
+  defmodule Server do
+    def get_connection do
+    end
+
+    def set(conn, key, val) do
+      "OK"
+    end
+
+    def get(conn, key) do
+    end
+  end
 end
--- test/remixdb_test.exs
+++ test/remixdb_test.exs
@@ -1,7 +1,26 @@
 defmodule RemixdbTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  defp set(key, val) do
+    import Remixdb.Server, only: [set: 3, get_connection: 0]
+    conn = get_connection
+    conn |> set(key, val)
+  end
+
+  defp get(key) do
+    import Remixdb.Server, only: [get: 2, get_connection: 0]
+    conn = get_connection
+    conn |> get(key)
+  end
+
+  test "set" do
+    res = set("foo", "bar")
+    assert res == "OK"
+  end
+
+  test "get" do
+    set "foo", "bar"
+    res = get "foo"
+    assert res == "bar"
   end
 end
