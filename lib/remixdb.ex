016273141f11e90768e8ec83839aad041168bab6
Santosh Kumar <santosh79@gmail.com>
2015-06-17 12:34:03 -0400

    set kinda working

--- lib/remixdb.ex
+++ lib/remixdb.ex
@@ -1,13 +1,37 @@
 defmodule Remixdb do
   defmodule Server do
+    def start do
+      server_pid = spawn fn -> loop() end
+      Process.register server_pid, :remixdb_server
+    end
+
     def get_connection do
     end
 
-    def set(conn, key, val) do
-      "OK"
+    def set(from, key, val) do
+      server_pid = Process.whereis :remixdb_server
+      send server_pid, {from, {:set, key, val}}
     end
 
-    def get(conn, key) do
+    def get(from, key) do
+      send from, "bar"
+    end
+
+    def stop(from) do
+      server_pid = Process.whereis :remixdb_server
+      send server_pid, {from, :die}
+    end
+
+    def loop do
+      receive do
+        {from, {:set, key, val}} ->
+          send from, :ok
+          loop()
+        {from, :die} ->
+          send from, :ok
+          Process.unregister :remixdb_server
+          Process.exit self, "asked to die by #{inspect from}"
+      end
     end
   end
 end
--- test/remixdb_test.exs
+++ test/remixdb_test.exs
@@ -2,9 +2,7 @@ defmodule RemixdbTest do
   use ExUnit.Case
 
   defp set(key, val) do
-    import Remixdb.Server, only: [set: 3, get_connection: 0]
-    conn = get_connection
-    conn |> set(key, val)
+    Remixdb.Server.set self, key, val
   end
 
   defp get(key) do
@@ -13,14 +11,43 @@ defmodule RemixdbTest do
     conn |> get(key)
   end
 
+  defp stop_server do
+    Remixdb.Server.stop self
+  end
+
+  defp start_server do
+    Remixdb.Server.start
+  end
+
+
+  test "start" do
+    assert Process.whereis(:remixdb_server) == nil
+    Remixdb.Server.start
+    server_pid = Process.whereis(:remixdb_server)
+    assert Process.alive?(server_pid)
+    stop_server()
+  end
+
+  test "stop" do
+    start_server()
+    Remixdb.Server.stop self
+    assert_receive :ok
+    server = Process.whereis(:remixdb_server)
+    assert server == nil
+  end
+
   test "set" do
-    res = set("foo", "bar")
-    assert res == "OK"
+    start_server()
+    Remixdb.Server.set self, "foo", "bar"
+    assert_receive :ok
+    stop_server()
   end
 
   test "get" do
+    start_server()
     set "foo", "bar"
-    res = get "foo"
-    assert res == "bar"
+    Remixdb.Server.get self, "foo"
+    assert_receive "bar"
+    stop_server()
   end
 end
