9c37b64f2fcabbb292107bf51a30518c1652a04a
Santosh Kumar <santosh79@gmail.com>
2015-10-21 02:16:56 -0400

    upgraded elixir version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Remixdb.Mixfile do
   def project do
     [app: :remixdb,
      version: "0.0.1",
-     elixir: "~> 1.0",
+     elixir: "~> 1.1",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps]
