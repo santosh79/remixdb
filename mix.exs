9bdec1327c2815a45c8e0231584d4dd22d586142
Santosh Kumar <santosh79@gmail.com>
2015-10-21 02:57:40 -0400

    upgraded exredis

--- mix.exs
+++ mix.exs
@@ -27,6 +27,6 @@ defmodule Remixdb.Mixfile do
   #
   # Type `mix help deps` for more examples and options
   defp deps do
-    [{ :exredis, ">= 0.1.1" }]
+    [{ :exredis, ">= 0.2.1" }]
   end
 end
