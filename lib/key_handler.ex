4af9f5861414392ced97a533e8bbc94ad4fdb468
Santosh Kumar <santosh79@gmail.com>
2015-10-24 02:26:06 -0400

    Cleaned up KeyHandler (a bit)

--- lib/key_handler.ex
+++ lib/key_handler.ex
@@ -1,14 +1,13 @@
 defmodule Remixdb.KeyHandler do
-  def get_key_pid(key) do
-    ("remixdb_string|" <> key) |> String.to_atom |> Process.whereis
-  end
-
   def get_or_create_key_pid(key) do
-    key_atom = ("remixdb_string|" <> key) |> String.to_atom
-    key_pid = case Process.whereis(key_atom) do
+    case (key |> get_key_pid) do
       nil -> Remixdb.String.start key
       pid -> pid
     end
+  end
+
+  def get_key_pid(key) do
+    ("remixdb_string|" <> key) |> String.to_atom |> Process.whereis
   end
 end
 
