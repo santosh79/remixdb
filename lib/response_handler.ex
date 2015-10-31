12b47ad0e4fc384b063e63ce593b04ba07804c61
Santosh Kumar <santosh79@gmail.com>
2015-10-31 06:36:05 -0400

    removed unneeded puts

--- lib/response_handler.ex
+++ lib/response_handler.ex
@@ -1,6 +1,5 @@
 defmodule Remixdb.ResponseHandler do
   def send_nil(socket) do
-    IO.puts "sending nil"
     :gen_tcp.send socket, "$-1\r\n"
     socket
   end
@@ -8,13 +7,11 @@ defmodule Remixdb.ResponseHandler do
   def send_val(socket, val) do
     val_bytes = val |> String.length |> Integer.to_string
     msg = "$" <> val_bytes <> "\r\n" <> val <> "\r\n"
-    IO.puts "sending val: #{msg}"
     :gen_tcp.send socket, msg
     socket
   end
 
   def send_ok(socket) do
-    IO.puts "sending ok"
     :gen_tcp.send socket, "+OK\r\n"
     socket
   end
