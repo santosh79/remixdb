a25d641b482710411284cfe64e238233645104a2
Santosh Kumar <santosh79@gmail.com>
2016-04-07 04:56:15 -0400

    Making tests DRY'er

--- test/remixdb_test.exs
+++ test/remixdb_test.exs
@@ -4,53 +4,48 @@ defmodule RemixdbTest do
 
     setup_all context do
       Remixdb.Server.start
-      :ok
+      client = Exredis.start_using_connection_string("redis://127.0.0.1:6379")
+      {:ok, %{client: client}}
     end
 
-    test "exists" do
-      client = Exredis.start_using_connection_string("redis://127.0.0.1:6379")
-      val = client |> Exredis.query ["EXISTS", "NON-EXISTENT-KEY"]
+    setup context do
+      %{client: client} = context
+      client |> Exredis.query(["FLUSHALL"])
+      {:ok, %{client: client}}
+    end
+
+    test "exists", %{client: client} do
+      val = client |> Exredis.query(["EXISTS", "NON-EXISTENT-KEY"])
       assert val === "0"
 
-      client |> Exredis.query ["SET", "FOO", "BARNED"]
-      val = client |> Exredis.query ["EXISTS", "FOO"]
-
+      client |> Exredis.query(["SET", "FOO", "BARNED"])
+      val = client |> Exredis.query(["EXISTS", "FOO"])
       assert val === "1"
-      client |> Exredis.stop
     end
 
-    test "set and get" do
-      client = Exredis.start_using_connection_string("redis://127.0.0.1:6379")
-      client |> Exredis.query ["SET", "FOO", "BARNED"]
-      val = client |> Exredis.query ["GET", "FOO"]
+    test "set and get", %{client: client} do
+      client |> Exredis.query(["SET", "FOO", "BARNED"])
+      val = client |> Exredis.query(["GET", "FOO"])
       assert val === "BARNED"
-      client |> Exredis.stop
     end
 
-    test "getset" do
-      client = Exredis.start_using_connection_string("redis://127.0.0.1:6379")
-      client |> Exredis.query ["FLUSHALL"]
-
-      val = client |> Exredis.query ["GETSET", "FOO", "HEY"]
+    test "getset", %{client: client} do
+      val = client |> Exredis.query(["GETSET", "FOO", "HEY"])
       assert val === :undefined
 
-      val = client |> Exredis.query ["GETSET", "FOO", "BYE"]
+      val = client |> Exredis.query(["GETSET", "FOO", "BYE"])
       assert val === "HEY"
 
-      val = client |> Exredis.query ["GET", "FOO"]
+      val = client |> Exredis.query(["GET", "FOO"])
       assert val === "BYE"
-      client |> Exredis.stop
     end
 
-    test "get non-existent key" do
-      client = Exredis.start_using_connection_string("redis://127.0.0.1:6379")
-      val = client |> Exredis.query ["GET", "NON-EXISTENT-KEY"]
+    test "get non-existent key", %{client: client} do
+      val = client |> Exredis.query(["GET", "NON-EXISTENT-KEY"])
       assert val === :undefined
-      client |> Exredis.stop
     end
 
-    test "dbsize" do
-      client = Exredis.start_using_connection_string("redis://127.0.0.1:6379")
+    test "dbsize", %{client: client} do
       prev_db_size = client |> Exredis.query(["DBSIZE"]) |> String.to_integer
       1..1_000 |> Enum.each(fn(x) ->
         key = "temp_key" <> (x |> Integer.to_string)
@@ -58,52 +53,42 @@ defmodule RemixdbTest do
       end)
       val = client |> Exredis.query(["DBSIZE"]) |> String.to_integer
       assert val === (prev_db_size + 1_000)
-      client |> Exredis.stop
     end
 
-    test "ping" do
-      client = Exredis.start_using_connection_string("redis://127.0.0.1:6379")
-      val = client |> Exredis.query ["PING"]
+    test "ping", %{client: client} do
+      val = client |> Exredis.query(["PING"])
       assert val === "PONG"
-      val = client |> Exredis.query ["PING", "hello world"]
+      val = client |> Exredis.query(["PING", "hello world"])
       assert val === "hello world"
     end
 
-    test "append - NON existing key" do
-      client = Exredis.start_using_connection_string("redis://127.0.0.1:6379")
-      client |> Exredis.query ["FLUSHALL"]
-
-      val = client |> Exredis.query ["APPEND", "foo", "bar"]
+    test "append - NON existing key", %{client: client} do
+      val = client |> Exredis.query(["APPEND", "foo", "bar"])
       assert val === "3"
 
-      val = client |> Exredis.query ["GET", "foo"]
+      val = client |> Exredis.query(["GET", "foo"])
       assert val === "bar"
     end
 
-    test "append - existing key" do
-      client = Exredis.start_using_connection_string("redis://127.0.0.1:6379")
-      client |> Exredis.query ["FLUSHALL"]
-
-      val = client |> Exredis.query ["SET", "mykey", "hello"]
-      val = client |> Exredis.query ["APPEND", "mykey", " world"]
+    test "append - existing key", %{client: client} do
+      val = client |> Exredis.query(["SET", "mykey", "hello"])
+      val = client |> Exredis.query(["APPEND", "mykey", " world"])
       assert val === "11"
 
-      val = client |> Exredis.query ["GET", "mykey"]
+      val = client |> Exredis.query(["GET", "mykey"])
       assert val === "hello world"
     end
 
-    test "flushall" do
-      client = Exredis.start_using_connection_string("redis://127.0.0.1:6379")
-      client |> Exredis.query ["SET", "A", "1"]
+    test "flushall", %{client: client} do
+      client |> Exredis.query(["SET", "A", "1"])
       db_sz = client |> Exredis.query(["DBSIZE"]) |> String.to_integer
       assert db_sz > 0
 
-      val = client |> Exredis.query ["FLUSHALL"]
+      val = client |> Exredis.query(["FLUSHALL"])
       assert val === "OK"
       new_db_sz = client |> Exredis.query(["DBSIZE"]) |> String.to_integer
       assert new_db_sz === 0
       # assert val === (prev_db_size + 2)
-      client |> Exredis.stop
     end
   end
 end
