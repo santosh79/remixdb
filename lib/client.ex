d63354de1d59445bb44ae46019afeeb0f7fcca1a
Santosh Kumar <santosh79@gmail.com>
2016-05-19 05:31:20 -0400

    cleanup in client using alias'es

--- lib/client.ex
+++ lib/client.ex
@@ -32,90 +32,91 @@ defmodule Remixdb.Client do
   end
 
   defp get_response() do
+    import Remixdb.KeyHandler, only: [flushall: 0, dbsize: 0, get_pid: 2, get_or_create_pid: 2, exists?: 1, rename_key: 2, renamenx_key: 2]
     receive do
       {:ping, []} -> "PONG"
       {:ping, [res]} -> res
-      :flushall -> Remixdb.KeyHandler.flushall
-      :dbsize -> Remixdb.KeyHandler.dbsize()
+      :flushall -> flushall
+      :dbsize -> dbsize()
       {:exists, [key]} ->
-        case Remixdb.KeyHandler.exists?(key) do
+        case exists?(key) do
           false -> 0
           true -> 1
         end
       {:append, [key, val]} ->
-        Remixdb.KeyHandler.get_or_create_pid(:string, key) |>
+        get_or_create_pid(:string, key) |>
         Remixdb.String.append(val)
       {:getset, [key, val]} ->
-        Remixdb.KeyHandler.get_or_create_pid(:string, key) |>
+        get_or_create_pid(:string, key) |>
         Remixdb.String.getset(val)
       {:set, [key, val]} ->
-         Remixdb.KeyHandler.get_or_create_pid(:string, key) |>
+        get_or_create_pid(:string, key) |>
         Remixdb.String.set(val)
       {:incrby, [key, val]} ->
-        Remixdb.KeyHandler.get_or_create_pid(:string, key) |>
+        get_or_create_pid(:string, key) |>
         Remixdb.String.incrby(val)
       {:decr, [key]} ->
-        Remixdb.KeyHandler.get_or_create_pid(:string, key) |>
+        get_or_create_pid(:string, key) |>
         Remixdb.String.decr
       {:decrby, [key, val]} ->
-        Remixdb.KeyHandler.get_or_create_pid(:string, key) |>
+        get_or_create_pid(:string, key) |>
         Remixdb.String.decrby(val)
       {:incr, [key]} ->
-        Remixdb.KeyHandler.get_or_create_pid(:string, key) |>
+        get_or_create_pid(:string, key) |>
         Remixdb.String.incr
       {:get, [key]} ->
-        Remixdb.KeyHandler.get_pid(:string, key) |>
+        get_pid(:string, key) |>
         Remixdb.String.get
       {:setex, [key, timeout, val]} ->
-        Remixdb.KeyHandler.get_or_create_pid(:string, key) |>
+        get_or_create_pid(:string, key) |>
         Remixdb.String.setex(timeout, val)
       {:ttl, [key]} ->
-        Remixdb.KeyHandler.get_pid(:string, key) |> Remixdb.String.ttl
+        get_pid(:string, key) |> Remixdb.String.ttl
       {:rename, [old_name, new_name]} ->
-        Remixdb.KeyHandler.rename_key(old_name, new_name)
+        rename_key(old_name, new_name)
       {:renamenx, [old_name, new_name]} ->
-        Remixdb.KeyHandler.renamenx_key(old_name, new_name)
+        renamenx_key(old_name, new_name)
       {:rpushx, [key|items]} ->
-        Remixdb.KeyHandler.get_pid(:list, key) |>
+        get_pid(:list, key) |>
         Remixdb.List.rpushx(items)
       {:rpush, [key|items]} ->
-        Remixdb.KeyHandler.get_or_create_pid(:list, key) |>
+        get_or_create_pid(:list, key) |>
         Remixdb.List.rpush(items)
       {:lpushx, [key|items]} ->
-        Remixdb.KeyHandler.get_pid(:list, key) |>
+        get_pid(:list, key) |>
         Remixdb.List.lpushx(items)
       {:lpush, [key|items]} ->
-        Remixdb.KeyHandler.get_or_create_pid(:list, key) |>
+        get_or_create_pid(:list, key) |>
         Remixdb.List.lpush(items)
       {:lpop, [key]} ->
-        Remixdb.KeyHandler.get_pid(:list, key) |> Remixdb.List.lpop
+        get_pid(:list, key) |> Remixdb.List.lpop
       {:rpop, [key]} ->
-        Remixdb.KeyHandler.get_pid(:list, key) |> Remixdb.List.rpop
+        get_pid(:list, key) |> Remixdb.List.rpop
       {:llen, [key]} ->
-        Remixdb.KeyHandler.get_pid(:list, key) |> Remixdb.List.llen
+        get_pid(:list, key) |> Remixdb.List.llen
       {:lrange, [key, start, stop]} ->
-        Remixdb.KeyHandler.get_pid(:list, key) |> Remixdb.List.lrange(start, stop)
+        get_pid(:list, key) |> Remixdb.List.lrange(start, stop)
       {:ltrim, [key, start, stop]} ->
-        Remixdb.KeyHandler.get_pid(:list, key) |> Remixdb.List.ltrim(start, stop)
+        get_pid(:list, key) |> Remixdb.List.ltrim(start, stop)
       {:lset, [key, idx, val]} ->
-        Remixdb.KeyHandler.get_pid(:list, key) |> Remixdb.List.lset(idx, val)
+        get_pid(:list, key) |> Remixdb.List.lset(idx, val)
       {:lindex, [key, idx]} ->
-        Remixdb.KeyHandler.get_pid(:list, key) |> Remixdb.List.lindex(idx)
+        get_pid(:list, key) |> Remixdb.List.lindex(idx)
       {:rpoplpush, [src, dest]} ->
-        src_pid  = Remixdb.KeyHandler.get_pid(:list, src)
-        dest_pid = Remixdb.KeyHandler.get_or_create_pid(:list, dest)
+        src_pid  = get_pid(:list, src)
+        dest_pid = get_or_create_pid(:list, dest)
         Remixdb.List.rpoplpush src_pid, dest_pid
       {:sadd, [key, val]} ->
-        Remixdb.KeyHandler.get_or_create_pid(:set, key) |>
+        get_or_create_pid(:set, key) |>
         Remixdb.Set.sadd(val)
       {:smembers, [key]} ->
-        Remixdb.KeyHandler.get_pid(:set, key) |>
+        get_pid(:set, key) |>
         Remixdb.Set.smembers
       {:sismember, [key, val]} ->
-        Remixdb.KeyHandler.get_pid(:set, key) |>
+        get_pid(:set, key) |>
         Remixdb.Set.sismember(val)
       {:scard, [key]} ->
-        Remixdb.KeyHandler.get_pid(:set, key) |>
+        get_pid(:set, key) |>
         Remixdb.Set.scard
     end
   end
