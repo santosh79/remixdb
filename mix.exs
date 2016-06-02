42ad288300c20bf6d873ee3d45d0b8dc205f0689
Santosh Kumar <santosh79@gmail.com>
2016-06-02 01:38:07 -0400

    specifying elixir version 1.2 in mix file

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Remixdb.Mixfile do
   def project do
     [app: :remixdb,
      version: "0.0.1",
-     # elixir: "~> 1.1",
+     elixir: "~> 1.2",
      default_task: "remixdb",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
