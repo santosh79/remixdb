e89a5f78aba03b4e5ce0cb3f253bf5505174f9fa
Santosh Kumar <santosh@coinbase.com>
2017-02-22 12:47:15 -0800

    using IO lists - performance

--- lib/response_handler.ex
+++ lib/response_handler.ex
@@ -9,26 +9,26 @@ defmodule Remixdb.ResponseHandler do
   end
 
   def send_response(socket, {:error, val}) when is_bitstring(val) do
-    msg = "-" <> val <> "\r\n"
+    msg = ["-", val, "\r\n"]
     :gen_tcp.send socket, msg
     socket
   end
 
   def send_response(socket, val) when is_bitstring(val) do
     val_bytes = val |> String.length |> Integer.to_string
-    msg = "$" <> val_bytes <> "\r\n" <> val <> "\r\n"
+    msg = ["$", val_bytes, "\r\n", val, "\r\n"]
     :gen_tcp.send socket, msg
     socket
   end
 
   def send_response(socket, num) when is_integer(num) do
-    response = ":" <> (num |> Integer.to_string) <> "\r\n"
+    response = [":", (num |> Integer.to_string), "\r\n"]
     :gen_tcp.send socket, response
     socket
   end
 
   def send_response(socket, val) when is_list(val) do
-    header = "*" <> (val |> Enum.count |> Integer.to_string) <> "\r\n"
+    header = ["*", (val |> Enum.count |> Integer.to_string), "\r\n"]
     :gen_tcp.send socket, header
     val |> Enum.each(fn(el) ->
       Remixdb.ResponseHandler.send_response(socket, el)
