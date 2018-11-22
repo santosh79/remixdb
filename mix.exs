76bcc52d1be4aeb2344037b933eaeecfe68ade66
Santosh Kumar <santosh79@gmail.com>
2018-11-22 10:35:59 -0800

    elixir version bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Remixdb.Mixfile do
   def project do
     [app: :remixdb,
      version: "0.0.1",
-     elixir: "~> 1.2",
+     elixir: "~> 1.5",
      default_task: "remixdb",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
