3eefa9e3fcd24fa96ab4bf6f383f67994bead96a
Santosh Kumar <santosh79@gmail.com>
2020-04-25 21:01:41 +0530

    Some minor benchmarking

--- lib/benchmark.ex
+++ lib/benchmark.ex
@@ -1,23 +1,23 @@
 defmodule Remixdb.Benchmark do
-  def simple_get_set(num_keys \\ 1_000_000) do
-    _ = Remixdb.Server.start_link
+  def get_and_set(num_elms \\ 1_000) do
     client = Exredis.start_using_connection_string("redis://127.0.0.1:6379")
     :timer.sleep 1_000
-    # keys and values
-    k_and_v = 1..num_keys
-              |> Enum.map(fn(_) ->
-                # rand number
-                rn = :rand.uniform(num_keys * 10)
-                key = "key-#{rn}"
-                val = "val-#{rn}"
-                {key, val}
-              end)
-    :timer.tc(fn ->
-      k_and_v |> Enum.map(fn({k, v}) ->
-        client |> Exredis.query(["SET", k, v])
-        val = client |> Exredis.query(["GET", k])
-        ^v = val
+
+    set_time = :timer.tc(fn ->
+      1..num_elms |> Enum.each(fn(x) ->
+        client |> Exredis.query(["SET", "FOO-#{x}", "BARNED-#{x}"])
       end)
     end)
+
+    get_time = :timer.tc(fn ->
+      1..num_elms |> Enum.each(fn(x) ->
+        "a" = "a"
+        val = "BARNED-#{x}"
+        ^val = Exredis.query(client, ["GET", "FOO-#{x}"])
+      end)
+    end)
+
+    %{:set_time => set_time, :get_time => get_time}
   end
 end
+
---
+++ lib/benchmark/datastructures.ex
@@ -0,0 +1,49 @@
+defmodule Remixdb.Benchmark.DataStructures do
+  def map_perf(num_elms \\ 1_000_000) when is_integer(num_elms) and num_elms > 1 do
+    el = :rand.uniform(num_elms)
+    :timer.tc(fn ->
+      mp_st = 1..num_elms |> Enum.reduce(%{}, fn(x, map) ->
+        Map.put(map, x, 1)
+      end)
+      1..num_elms |> Enum.each(fn(x) ->
+        1 == Map.get(mp_st, x)
+      end)
+    end)
+  end
+
+  def set_perf(num_elms) when is_integer(num_elms) and num_elms > 1 do
+    mp_st = 1..num_elms |> Enum.into(MapSet.new())
+    el = :rand.uniform(num_elms)
+    :timer.tc(MapSet, :member?, [mp_st, el])
+  end
+
+  def list_perf(num_elms) when is_integer(num_elms) and num_elms > 1 do
+    lst = 1..num_elms |> Enum.into([])
+    :timer.tc(fn ->
+      el = :rand.uniform(num_elms)
+      lst |> Enum.any?(fn(x) ->
+        x == el
+      end)
+    end)
+  end
+
+  def sets_vs_maps(num_elms \\ 1_000_000) when is_integer(num_elms) and num_elms > 1 do
+    set_task = Task.async(__MODULE__, :set_perf, [num_elms])
+    map_task = Task.async(__MODULE__, :map_perf, [num_elms])
+
+    set_result = Task.await(set_task, :timer.seconds(10))
+    map_result = Task.await(map_task, :timer.seconds(10))
+
+    [{:map_time, map_result}, {:set_time, set_result}]
+  end
+
+  def sets_vs_list(num_elms) when is_integer(num_elms) and num_elms > 1 do
+    set_task = Task.async(__MODULE__, :set_perf, [num_elms])
+    list_task = Task.async(__MODULE__, :list_perf, [num_elms])
+
+    set_result = Task.await(set_task)
+    lst_result = Task.await(list_task)
+
+    [{:list_time, lst_result}, {:set_time, set_result}]
+  end
+end
