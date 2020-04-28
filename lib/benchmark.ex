d8673308c2070c9c54b21c1f55ad5ddb4758ca80
Santosh Kumar <santosh79@gmail.com>
2020-04-28 09:55:53 +0530

    Benchmark changes
    - creating concurrent connections

--- lib/benchmark.ex
+++ lib/benchmark.ex
@@ -1,12 +1,17 @@
 defmodule Remixdb.Benchmark do
-  def get_and_set(num_elms \\ 1_000) do
-    client = Exredis.start_using_connection_string("redis://127.0.0.1:6379")
+  def get_and_set(num_elms \\ 1_000, num_clients \\ 50) do
+    clients = 1..num_clients
+              |> Enum.map(fn(_) ->
+                Exredis.start_using_connection_string("redis://127.0.0.1:6379")
+              end)
     :timer.sleep 1_000
 
     set_time = :timer.tc(fn ->
       1..num_elms
       |> Enum.map(fn(x) ->
         Task.async(fn ->
+          idx = rem(x, num_clients)
+          client = clients |> Enum.at(idx)
           client |> Exredis.query(["SET", "FOO-#{x}", "BARNED-#{x}"])
         end)
       end)
@@ -19,6 +24,8 @@ defmodule Remixdb.Benchmark do
         Task.async(fn ->
           "a" = "a"
           val = "BARNED-#{x}"
+          idx = rem(x, num_clients)
+          client = clients |> Enum.at(idx)
           ^val = Exredis.query(client, ["GET", "FOO-#{x}"])
         end)
       end)
