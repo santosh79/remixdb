b0ddf3bfd8f1636ab72a71b245273f45344a1d38
Santosh Kumar <santosh79@gmail.com>
2021-04-05 13:36:55 +0530

    better benchmarking

--- lib/benchmark/parsers.ex
+++ lib/benchmark/parsers.ex
@@ -1,22 +1,55 @@
 defmodule Remixdb.Benchmark.Parsers do
-  def redis_parser_bm(num_items \\ 1_000) do
+
+  def redis_parser_bm(num_items \\ 1_000, num_workers \\ 50) do
     :timer.sleep 1_000
     client = Exredis.start_using_connection_string("redis://127.0.0.1:6379")
     :timer.sleep 1_000
     keys_and_vals = 1..num_items
                     |> Enum.map(&( {"key: #{&1}", "val: #{&1}"} ))
 
-    result = :timer.tc(fn ->
-      t_ids = keys_and_vals |> Enum.map(fn({k, v}) ->
-        Task.async __MODULE__, :set_task, [client, k, v]
+    worker_buckets = keys_and_vals |> Enum.chunk_every(num_workers)
+
+    set_result = :timer.tc(fn ->
+      t_ids = worker_buckets |> Enum.map(fn(bucket) ->
+        Task.async(fn ->
+          tts = bucket |> Enum.map(fn({k, v}) ->
+            Task.async __MODULE__, :set_task, [client, k, v]
+          end)
+
+          tts |> Enum.each(fn(t_id) ->
+            Task.await t_id
+          end)
+        end)
       end)
 
       t_ids |> Enum.each(fn(t_id) ->
         Task.await t_id
       end)
+
     end)
+
+    get_result = :timer.tc(fn ->
+      t_ids = worker_buckets |> Enum.map(fn(bucket) ->
+        Task.async(fn ->
+          tts = bucket |> Enum.map(fn({k, v}) ->
+            Task.async __MODULE__, :get_task, [client, k, v]
+          end)
+
+          tts |> Enum.each(fn(t_id) ->
+            Task.await t_id
+          end)
+        end)
+      end)
+
+      t_ids |> Enum.each(fn(t_id) ->
+        Task.await t_id
+      end)
+
+    end)
+
     :ok = client |> Exredis.stop()
-    :io.format("~n~n ~p with result: ~p ~n~n", [__MODULE__, result])
+
+    :io.format("~n~n ~p with num_items: ~p and num_workers ~p, result, set -- ~p, get -- ~p ~n~n", [__MODULE__, num_items, num_workers, set_result, get_result])
     :timer.sleep(:timer.seconds(2))
     Process.exit(self(), :kill)
   end
@@ -24,6 +57,10 @@ defmodule Remixdb.Benchmark.Parsers do
 
   def set_task(client, k, v) do
     "OK" = client |> Exredis.query(["SET", k, v])
+  end
+
+  def get_task(client, k, v) do
+    v = client |> Exredis.query(["SET", k, v])
   end
 end
 
