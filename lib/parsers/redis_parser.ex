ac9341e77aa8542bf0b2f101a0c2d7ec2f594e56
Santosh Kumar <santosh79@gmail.com>
2023-12-20 06:48:22 -0800

    small cleanup in parser

--- lib/parsers/redis_parser.ex
+++ lib/parsers/redis_parser.ex
@@ -1,7 +1,8 @@
 defmodule Remixdb.Parsers.RedisParser do
   use GenServer
+
   def start_link(socket) do
-    GenServer.start_link __MODULE__, {:ok, socket}, []
+    GenServer.start_link(__MODULE__, {:ok, socket}, [])
   end
 
   def init({:ok, socket}) do
@@ -9,46 +10,47 @@ defmodule Remixdb.Parsers.RedisParser do
   end
 
   def read_command(pid) do
-    GenServer.call pid, :read_command, :infinity
+    GenServer.call(pid, :read_command, :infinity)
   end
 
   def handle_call(:read_command, _from, socket = state) do
-    {:ok, [cmd|args]} = read_new_command(socket)
+    {:ok, [cmd | args]} = read_new_command(socket)
     res = parse_command(cmd, args)
     response = {:ok, res}
     {:reply, response, state}
   end
+
   def handle_info(_, state), do: {:noreply, state}
 
   defp read_new_command(socket) do
     {:ok, num_args} = read_number_args(socket)
-    read_args socket, num_args
+    read_args(socket, num_args)
   end
 
   defp read_args(socket, num) do
-    read_args socket, num, []
+    read_args(socket, num, [])
   end
 
   defp read_args(_socket, 0, accum) do
-    {:ok, accum}
+    {:ok, :lists.reverse(accum)}
   end
+
   defp read_args(socket, num, accum) do
     {:ok, num_bytes} = read_bytes(socket)
-    {:ok, data} = read_line socket
+    {:ok, data} = read_line(socket)
 
     # Ensure the number of bytes matches
     # Subtract 2 for the trailing /r/n
-    ^num_bytes = (:erlang.byte_size(data) - 2)
+    ^num_bytes = :erlang.byte_size(data) - 2
 
     msg = data |> :binary.part(0, :erlang.byte_size(data) - 2)
 
-    read_args socket, (num - 1), (accum ++ [msg])
+    read_args(socket, num - 1, [msg | accum])
   end
 
   defp read_bytes(socket) do
-    {:ok, data}  = read_line socket
-    dd = data |> line_to_int
-    {:ok, dd}
+    {:ok, data} = read_line(socket)
+    {:ok, line_to_int(data)}
   end
 
   defp read_line(socket) do
@@ -65,69 +67,68 @@ defmodule Remixdb.Parsers.RedisParser do
   end
 
   defp line_to_int(data) do
-    data |> :binary.part(1, :erlang.byte_size(data) - 3) |> :erlang.binary_to_integer
+    data |> :binary.part(1, :erlang.byte_size(data) - 3) |> :erlang.binary_to_integer()
   end
 
   defp parse_command(cmd, args) do
-    case (cmd |> String.upcase) do
-      "CONFIG"      -> {:config, args}
-      "SET"         -> {:set, args}
-      "APPEND"      -> {:append, args}
-      "GET"         -> {:get, args}
-      "GETSET"      -> {:getset, args}
-      "EXISTS"      -> {:exists, args}
-      "DBSIZE"      -> :dbsize
-      "FLUSHALL"    -> :flushall
-      "PING"        -> {:ping, args}
-      "INCR"        -> {:incr, args}
-      "DECR"        -> {:decr, args}
-      "DECRBY"      -> {:decrby, args}
-      "INCRBY"      -> {:incrby, args}
-      "SETEX"       -> {:setex, args}
-      "TTL"         -> {:ttl, args}
-      "RENAME"      -> {:rename, args}
-      "RENAMENX"    -> {:renamenx, args}
-      "RPUSH"       -> {:rpush, args}
-      "RPUSHX"      -> {:rpushx, args}
-      "LPUSH"       -> {:lpush, args}
-      "LPUSHX"      -> {:lpushx, args}
-      "LPOP"        -> {:lpop, args}
-      "RPOP"        -> {:rpop, args}
-      "LLEN"        -> {:llen, args}
-      "LRANGE"      -> {:lrange, args}
-      "LTRIM"       -> {:ltrim, args}
-      "LSET"        -> {:lset, args}
-      "LINDEX"      -> {:lindex, args}
-      "RPOPLPUSH"   -> {:rpoplpush, args}
-      "SADD"        -> {:sadd, args}
-      "SREM"        -> {:srem, args}
-      "SMEMBERS"    -> {:smembers, args}
-      "SISMEMBER"   -> {:sismember, args}
-      "SMISMEMBER"  -> {:smismember, args}
-      "SCARD"       -> {:scard, args}
-      "SUNION"      -> {:sunion, args}
-      "SDIFF"       -> {:sdiff, args}
-      "SINTER"      -> {:sinter, args}
+    case cmd |> String.upcase() do
+      "CONFIG" -> {:config, args}
+      "SET" -> {:set, args}
+      "APPEND" -> {:append, args}
+      "GET" -> {:get, args}
+      "GETSET" -> {:getset, args}
+      "EXISTS" -> {:exists, args}
+      "DBSIZE" -> :dbsize
+      "FLUSHALL" -> :flushall
+      "PING" -> {:ping, args}
+      "INCR" -> {:incr, args}
+      "DECR" -> {:decr, args}
+      "DECRBY" -> {:decrby, args}
+      "INCRBY" -> {:incrby, args}
+      "SETEX" -> {:setex, args}
+      "TTL" -> {:ttl, args}
+      "RENAME" -> {:rename, args}
+      "RENAMENX" -> {:renamenx, args}
+      "RPUSH" -> {:rpush, args}
+      "RPUSHX" -> {:rpushx, args}
+      "LPUSH" -> {:lpush, args}
+      "LPUSHX" -> {:lpushx, args}
+      "LPOP" -> {:lpop, args}
+      "RPOP" -> {:rpop, args}
+      "LLEN" -> {:llen, args}
+      "LRANGE" -> {:lrange, args}
+      "LTRIM" -> {:ltrim, args}
+      "LSET" -> {:lset, args}
+      "LINDEX" -> {:lindex, args}
+      "RPOPLPUSH" -> {:rpoplpush, args}
+      "SADD" -> {:sadd, args}
+      "SREM" -> {:srem, args}
+      "SMEMBERS" -> {:smembers, args}
+      "SISMEMBER" -> {:sismember, args}
+      "SMISMEMBER" -> {:smismember, args}
+      "SCARD" -> {:scard, args}
+      "SUNION" -> {:sunion, args}
+      "SDIFF" -> {:sdiff, args}
+      "SINTER" -> {:sinter, args}
       "SRANDMEMBER" -> {:srandmember, args}
-      "SPOP"        -> {:spop, args}
-      "SMOVE"       -> {:smove, args}
-      "SDIFFSTORE"  -> {:sdiffstore, args}
+      "SPOP" -> {:spop, args}
+      "SMOVE" -> {:smove, args}
+      "SDIFFSTORE" -> {:sdiffstore, args}
       "SUNIONSTORE" -> {:sunionstore, args}
       "SINTERSTORE" -> {:sinterstore, args}
-      "HSET"        -> {:hset, args}
-      "HMSET"       -> {:hmset, args}
-      "HSETNX"      -> {:hsetnx, args}
-      "HMGET"       -> {:hmget, args}
-      "HLEN"        -> {:hlen, args}
-      "HGET"        -> {:hget, args}
-      "HGETALL"     -> {:hgetall, args}
-      "HDEL"        -> {:hdel, args}
-      "HKEYS"       -> {:hkeys, args}
-      "HVALS"       -> {:hvals, args}
-      "HEXISTS"     -> {:hexists, args}
-      "HSTRLEN"     -> {:hstrlen, args}
-      "HINCRBY"     -> {:hincrby, args}
+      "HSET" -> {:hset, args}
+      "HMSET" -> {:hmset, args}
+      "HSETNX" -> {:hsetnx, args}
+      "HMGET" -> {:hmget, args}
+      "HLEN" -> {:hlen, args}
+      "HGET" -> {:hget, args}
+      "HGETALL" -> {:hgetall, args}
+      "HDEL" -> {:hdel, args}
+      "HKEYS" -> {:hkeys, args}
+      "HVALS" -> {:hvals, args}
+      "HEXISTS" -> {:hexists, args}
+      "HSTRLEN" -> {:hstrlen, args}
+      "HINCRBY" -> {:hincrby, args}
     end
   end
 end
-
